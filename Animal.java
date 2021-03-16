e007a06a71433a0fbfc63319a75f9e43b127e13c
Batyrkhan <55399621+I3Fly@users.noreply.github.com>
2021-03-16 13:14:16 +0600

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public interface Animal  {
+
+        public void animalSound();
+
+        public void sleep();
+
+    }
+
+    class Pig implements Animal {
+        public void animalSound() {
+
+            System.out.println("The pig says: wee wee");
+
+        }
+        public void sleep() {
+
+            System.out.println("Zzz");
+
+        }
+    }
+
+    class Main {
+        public static void main(String[] args) {
+            Pig pig = new Pig();
+            pig.animalSound();
+            pig.sleep();
+        }
+}
